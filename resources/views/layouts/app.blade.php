b3a287330fedc934c9fe29d1f77b4100e1c2365d
zahir <zahirzohair445@gmail.com>
2020-05-05 00:44:45 +0800

    background image and items added in welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #800000">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'KiahStore') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,6 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Feature</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Price</a>
+                        </li>
 
                     </ul>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,189 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <div class="container">
+        <!--Carousel Wrapper-->
+        <div id="carousel-thumb" class="carousel slide carousel-fade carousel-thumbnails" data-ride="carousel">
+            <!--Slides-->
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="{{ asset('img/1.jpg') }}" alt="First slide">
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    KiahStore. It is almost alive.
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="{{ asset('img/2.jpg') }}" alt="Second slide">
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="{{ asset('img/3.jpeg') }}"alt="Third slide">
                 </div>
             </div>
+            <!--/.Slides-->
+            <!--Controls-->
+              <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            <!--/.Controls-->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-thumb" data-slide-to="0" class="active">
+                  <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(88).jpg" width="100">
+                </li>
+                <li data-target="#carousel-thumb" data-slide-to="1">
+                  <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(121).jpg" width="100">
+                </li>
+                <li data-target="#carousel-thumb" data-slide-to="2">
+                  <img src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(31).jpg" width="100">
+                </li>
+            </ol>
         </div>
-    </body>
-</html>
+        <!--/.Carousel Wrapper-->
+
+        <section class="text-center my-5">
+          <!-- Grid row -->
+            <div class="row">
+
+                <!-- Grid column -->
+                <div class="col-lg-3 col-md-6 mb-lg-0 mb-4">
+                  <!-- Card -->
+                    <div class="card align-items-center">
+                        <!-- Card image -->
+                        <div class="view overlay">
+                          <img src="{{ asset('img/blue.jpg') }}" class="card-img-top" alt="">
+                          <a>
+                            <div class="mask rgba-white-slight"></div>
+                          </a>
+                        </div>
+                        <!-- Card image -->
+                        <!-- Card content -->
+                        <div class="card-body text-center">
+                          <!-- Category & Title -->
+                          <a href="" class="grey-text">
+                            <h5>Shirt</h5>
+                          </a>
+                          <h5>
+                            <strong>
+                              <a href="" class="dark-grey-text">Denim shirt
+                                <span class="badge badge-pill badge-danger">NEW</span>
+                              </a>
+                            </strong>
+                          </h5>
+                          <h4 class="font-weight-bold blue-text">
+                            <strong>120$</strong>
+                          </h4>
+                        </div>
+                        <!-- Card content -->
+                    </div>
+                  <!-- Card -->
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-lg-3 col-md-6 mb-lg-0 mb-4">
+                  <!-- Card -->
+                    <div class="card align-items-center">
+                        <!-- Card image -->
+                        <div class="view overlay">
+                          <img src="{{ asset('img/grey.jpg') }}" class="card-img-top" alt="">
+                          <a>
+                            <div class="mask rgba-white-slight"></div>
+                          </a>
+                        </div>
+                        <!-- Card image -->
+                        <!-- Card content -->
+                        <div class="card-body text-center">
+                          <!-- Category & Title -->
+                          <a href="" class="grey-text">
+                            <h5>Sport wear</h5>
+                          </a>
+                          <h5>
+                            <strong>
+                              <a href="" class="dark-grey-text">Sweatshirt</a>
+                            </strong>
+                          </h5>
+                          <h4 class="font-weight-bold blue-text">
+                            <strong>139$</strong>
+                          </h4>
+                        </div>
+                        <!-- Card content -->
+                    </div>
+                  <!-- Card -->
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-lg-3 col-md-6 mb-md-0 mb-4">
+                  <!-- Card -->
+                    <div class="card align-items-center">
+                        <!-- Card image -->
+                        <div class="view overlay">
+                          <img src="{{ asset('img/red.jpg') }}" class="card-img-top" alt="">
+                          <a>
+                            <div class="mask rgba-white-slight"></div>
+                          </a>
+                        </div>
+                        <!-- Card image -->
+                        <!-- Card content -->
+                        <div class="card-body text-center">
+                          <!-- Category & Title -->
+                          <a href="" class="grey-text">
+                            <h5>Sport wear</h5>
+                          </a>
+                          <h5>
+                            <strong>
+                              <a href="" class="dark-grey-text">Grey blouse
+                                <span class="badge badge-pill badge-primary">BEST</span>
+                              </a>
+                            </strong>
+                          </h5>
+                          <h4 class="font-weight-bold blue-text">
+                            <strong>99$</strong>
+                          </h4>
+                        </div>
+                        <!-- Card content -->
+                    </div>
+                   <!-- Card -->
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-lg-3 col-md-6">
+                  <!-- Card -->
+                    <div class="card align-items-center">
+                        <!-- Card image -->
+                        <div class="view overlay">
+                          <img src="{{ asset('img/coat.jpg') }}" class="card-img-top" alt="">
+                          <a>
+                            <div class="mask rgba-white-slight"></div>
+                          </a>
+                        </div>
+                        <!-- Card image -->
+                        <!-- Card content -->
+                        <div class="card-body text-center">
+                          <!-- Category & Title -->
+                          <a href="" class="grey-text">
+                            <h5>Outwear</h5>
+                          </a>
+                          <h5>
+                            <strong>
+                              <a href="" class="dark-grey-text">Black jacket</a>
+                            </strong>
+                          </h5>
+                          <h4 class="font-weight-bold blue-text">
+                            <strong>219$</strong>
+                          </h4>
+                        </div>
+                        <!-- Card content -->
+                    </div>
+                    <!-- Card -->
+                </div>
+                <!-- Grid column -->
+            </div>
+          <!-- Grid row -->
+        </section>
+    </div>
+@endsection 
+
