9e476ecc6a989935b2cd5f91508c4e7e96a05360
bjoe88 <bjoe88@gmail.com>
2020-05-26 03:29:55 +0800

    Add table to admin shop owner and status column to shop page.

--- resources/views/admin/shopOwner.blade.php
+++ resources/views/admin/shopOwner.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container shop-owner">
-  <table id="example" class="table table-bordered table-sm mt-4">
+  <table id="shop-owner-table" class="table table-bordered table-sm mt-4">
     <thead class="thead thead-dark">
       <tr>
         <th>Id</th>
@@ -14,11 +14,31 @@
     </thead>
     <tbody>
       <tr>
-        <td>1</td>
+        <td>2</td>
         <td>JomFun</td>
         <td>Approve</td>
         <td>12 May 2020</td>
-        <td> 
+        <td>
+          <button>Enable</button>
+          <button>Disable</button>
+        </td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>123</td>
+        <td>Approve</td>
+        <td>12 May 2020</td>
+        <td>
+          <button>Enable</button>
+          <button>Disable</button>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>abc</td>
+        <td>Approve</td>
+        <td>12 May 2020</td>
+        <td>
           <button>Enable</button>
           <button>Disable</button>
         </td>
@@ -26,4 +46,18 @@
     </tbody>
   </table>
 </div>
+@endsection
+@section('scripts')
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.css" />
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.js"></script>
+<script>
+  jQuery(document).ready(function($) {
+    var table = $('#shop-owner-table').DataTable({
+      orderCellsTop: true,
+      fixedHeader: true,
+      order: [0, 'desc']
+
+    });
+  });
+</script>
 @endsection
