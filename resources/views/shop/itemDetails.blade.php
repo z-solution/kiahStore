4ca9695d8f94c7cd10f4952c6c64427b9a58117b
Muhammad Azizol Aminuddin <bjoe88@gmail.com>
2020-06-07 02:49:20 +0800

    Change product detail page

--- resources/views/shop/itemDetails.blade.php
+++ resources/views/shop/itemDetails.blade.php
@@ -13,10 +13,10 @@
                     </div> <!-- slider-product.// -->
                     <div class="img-small-wrap">
                         @forelse ($product->attachment as $attachment)
-                            <div class="item-gallery">
-                                <img class="product-image" src="/{{ $attachment->filename }}">
-                            </div>
-                        @empty  
+                        <div class="item-gallery">
+                            <img class="product-image" src="/{{ $attachment->filename }}">
+                        </div>
+                        @empty
                         <div class="item-gallery">
                             <img class="product-image" src="{{ $product->getFirstAttachmentFilename() }}">
                         </div>
@@ -27,17 +27,6 @@
             <aside class="col-sm-7">
                 <article class="card-body p-5">
                     <h3 class="title mb-3">{{ $product->name }}</h3>
-
-                    <div class="rating">
-                        <div class="stars">
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star"></span>
-                            <span class="fa fa-star"></span>
-                        </div>
-                    </div>
-
                     <p class="price-detail-wrap">
                         <span class="price h3 text-primary">
                             <span class="currency">MYR </span><span class="num">{{ $product->price }}</span>
@@ -49,64 +38,28 @@
                             <p>{{ $product->description }} </p>
                         </dd>
                     </dl>
-
-                    <dl class="param param-feature">
-                        <dt>Color: </dt>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                            <label class="form-check-label" for="inlineCheckbox1">Black</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                            <label class="form-check-label" for="inlineCheckbox2">White</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                            <label class="form-check-label" for="inlineCheckbox2">Blue</label>
-                        </div>
-                    </dl> <!-- item-property-hor .// -->
                     <hr>
                     <div class="row">
                         <div class="col-sm-4 col-sm-offset-4 mr-4">
                             <label>Quantity:</label>
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
-                                    <button class="btn btn-dark btn-sm" id="minus-btn"><i
-                                            class="fa fa-minus"></i></button>
+                                    <button class="btn btn-dark btn-sm" id="minus-btn"><i class="fa fa-minus"></i></button>
                                 </div>
-                                <input type="number" id="qty_input" class="form-control form-control-sm" value="1"
-                                    min="1">
+                                <input type="number" id="qty_input" class="form-control form-control-sm" value="1" min="1">
                                 <div class="input-group-prepend">
-                                    <button class="btn btn-dark btn-sm" id="plus-btn"><i
-                                            class="fa fa-plus"></i></button>
+                                    <button class="btn btn-dark btn-sm" id="plus-btn"><i class="fa fa-plus"></i></button>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-7">
                             <dl class="param param-inline">
-                                <dt class="mb-2">Size: </dt>
-                                <dd>
-                                    <label class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio2" value="option2">
-                                        <span class="form-check-label">S</span>
-                                    </label>
-                                    <label class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio2" value="option2">
-                                        <span class="form-check-label">M</span>
-                                    </label>
-                                    <label class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio2" value="option2">
-                                        <span class="form-check-label">L</span>
-                                    </label>
-                                    <label class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                            id="inlineRadio2" value="option2">
-                                        <span class="form-check-label">XL</span>
-                                    </label>
-                                </dd>
+                                <dt class="mb-2">Variants: </dt>
+                                <select name="" id="">
+                                    @foreach($product->inventoryVariant as $variant)
+                                    <option value="{{ $variant->id }}">{{ $variant->name }}</option>
+                                    @endforeach
+                                </select>
                             </dl> <!-- item-property .// -->
                         </div> <!-- col.// -->
                     </div> <!-- row.// -->
@@ -124,12 +77,12 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.css" />
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/datatables.min.js"></script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         $('#qty_input').prop('disabled', true);
-        $('#plus-btn').click(function () {
+        $('#plus-btn').click(function() {
             $('#qty_input').val(parseInt($('#qty_input').val()) + 1);
         });
-        $('#minus-btn').click(function () {
+        $('#minus-btn').click(function() {
             $('#qty_input').val(parseInt($('#qty_input').val()) - 1);
             if ($('#qty_input').val() == 0) {
                 $('#qty_input').val(1);
@@ -140,6 +93,5 @@
             $('#product-image-main-display').attr('src', $(ele.currentTarget).attr('src'));
         })
     });
-
 </script>
 @endsection
