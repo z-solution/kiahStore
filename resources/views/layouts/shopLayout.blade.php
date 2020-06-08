6caf38306d4f0ba3649865b89e0d9d463918582c
Zahir Zohair <zahirzohair445@gmail.com>
2020-06-08 14:40:52 +0800

    shopsite navbar extra featurs removed

--- resources/views/layouts/shopLayout.blade.php
+++ resources/views/layouts/shopLayout.blade.php
@@ -33,14 +33,14 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <!-- <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Shop Owner</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Contact</a>
                         </li>
-                    </ul>
+                    </ul> -->
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
